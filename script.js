6d09b39866e39089ab2d6f676d4f8cafa9aeabc1
PietroFDEV <pi_favoreto@yahoo.com.br>
2021-12-02 19:45:48 -0300

    Iniciando a construir

---
+++ script.js
@@ -0,0 +1,12 @@
+function piscar(){
+    document.getElementById("pisca").innerHTML = "^";
+}
+function volta(){
+    document.getElementById("pisca").innerHTML = "O";
+}
+function piscar2(){
+    document.getElementById("pisca2").innerHTML = "^";
+}
+function volta2(){
+    document.getElementById("pisca2").innerHTML = "O";
+}
